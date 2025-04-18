a37a56b9dd298c265cd57ace3a2513948bce58b9
Samantha Harper <sharper23@ivytech.edu>
2025-04-18 12:24:52 -0400

    teacher dir created w/ page.js
    This page will populate once the teacher's login is verified.

---
+++ course-manager/src/app/teacherDash/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Export the page component as the default export of this file
+export default function TeacherDashboard() {
+    return (
+        <div className="container text-center mt-5">
+        <h1 className="text-gray-700 text-2xl font-bold">Welcome to Teacher Dashboard</h1>
+        <p>Here you can modify and manage your courses.</p>
+        
+      </div>
+    );
+  }
