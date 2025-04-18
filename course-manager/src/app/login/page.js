ae15181fed2b88fa920fef233641d5e9a01c70b4
Samantha Harper <sharper23@ivytech.edu>
2025-04-18 12:07:27 -0400

    Added directory and page.js files for nav links
    Each nav link now pulls a new page:
    courses
    login
    -- Home reverts back to main page (the index of this file) .

---
+++ course-manager/src/app/login/page.js
@@ -0,0 +1,12 @@
+// Export the page component as the default export of this file
+export default function Page() {
+    return (
+       // main element for page with H1 and styling similiar to M05 tutorial with tailwind
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>
+           Login for student and teachers to come soon! 
+        </h1>
+  
+      </main>
+    );
+  }
