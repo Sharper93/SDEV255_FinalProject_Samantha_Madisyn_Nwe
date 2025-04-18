eb00dfbb3adc1d484a5da0df2b152902646761b3
Samantha Harper <sharper23@ivytech.edu>
2025-04-18 12:38:00 -0400

    Temp Link to login page
    The Login Page has a temporary link for teachers that sends to the teacher page -- this is needed until validation for login is made.
    
    A button was also made for students, but it does not go anywhere at this time.

--- course-manager/src/app/login/page.js
+++ course-manager/src/app/login/page.js
@@ -1,3 +1,11 @@
+// Import React library to build components
+import React from "react";
+// Import the Next.js Image component for handling images efficiently
+import Image from "next/image";
+import Link from "next/link"; // added link import from next.js for navigation between pages
+// Import Bootstrap CSS for pre-defined styling
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // Export the page component as the default export of this file
 export default function Page() {
     return (
@@ -6,6 +14,16 @@ export default function Page() {
         <h1>
            Login for student and teachers to come soon! 
         </h1>
+
+        <button className="flex items-center">
+          {/* TEMPORARY LINK to teacher page */}
+          <Link href="" className="text-primary font-bold bg-blue-200 p-3 my-2 border-2 border-transparent hover:border-blue-500">Student DashBoard</Link>
+        </button>
+
+        <button className="flex items-center">
+          {/* TEMPORARY LINK to teacher page */}
+          <Link href="./teacherDash" className="text-primary font-bold bg-blue-200 p-3 my-2 border-2 border-transparent hover:border-blue-500">Teacher DashBoard</Link>
+        </button>
   
       </main>
     );
--- course-manager/src/app/teacherDash/page.js
+++ course-manager/src/app/teacherDash/page.js
@@ -1,12 +1,23 @@
 import React from "react";
+import Link from "next/link"; // added link import from next.js for navigation between pages
 
 // Export the page component as the default export of this file
-export default function TeacherDashboard() {
+export default function Page() {
     return (
         <div className="container text-center mt-5">
         <h1 className="text-gray-700 text-2xl font-bold">Welcome to Teacher Dashboard</h1>
         <p>Here you can modify and manage your courses.</p>
-        
+
+        <button type="viewYourCourses" 
+          className="text-primary font-bold bg-blue-200 p-3 my-2 border-2 border-transparent hover:border-blue-500">
+          View Courses
+        </button> 
+
+        <button type="addCourse" 
+          className="text-primary font-bold bg-blue-200 p-3 my-2 border-2 border-transparent hover:border-blue-500">
+          Add Course
+        </button>
+
       </div>
     );
   }
